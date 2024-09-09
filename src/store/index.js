3629135bd40e659aaaafd0f24f53337fc4507c17
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-09-09 15:45:01 +0200

    feat: split reducer splices in multiple files

--- src/store/index.js
+++ src/store/index.js
@@ -1,59 +1,7 @@
 // import { createStore } from "redux";
-import { createSlice, configureStore } from "@reduxjs/toolkit";
-
-const initialCounterState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const authSlice = createSlice({
-  name: "auth",
-  initialState: { isLoggedIn: false },
-  reducers: {
-    login(state) {
-      state.isLoggedIn = true;
-    },
-    logout(state) {
-      state.isLoggedIn = false;
-    },
-  },
-});
-
-// const counterReducer = (state = initialState, action) => {
-//   if (action.type === "increment") {
-//     return { counter: state.counter + 1, showCounter: state.showCounter };
-//   }
-//   if (action.type === "increase") {
-//     return {
-//       counter: state.counter + action.amount,
-//       showCounter: state.showCounter,
-//     };
-//   }
-//   if (action.type === "decrement") {
-//     return { counter: state.counter - 1, showCounter: state.showCounter };
-//   }
-
-//   if (action.type === "toggle") {
-//     return { ...state, showCounter: !state.showCounter };
-//   }
-//   return state;
-// };
+import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "./counter-slice";
+import authSlice from "./auth-slice";
 
 const store = configureStore({
   reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
